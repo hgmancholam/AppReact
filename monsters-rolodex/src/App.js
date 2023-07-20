8a962992904f17d53936d5eb7063106a26d40911
hgmancholam <hgmancholam@gmail.com>
2023-07-20 14:35:22 -0500

    adding components

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import CardList from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
@@ -53,13 +53,7 @@ class App extends Component {
           onChange={onSearchChange}
         />
         <br />
-        {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
